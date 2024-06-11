a5d509346e335af68ae4dbd52b446798709a5b48
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-11 17:36:27 -0400

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """Main application entry point"""
-from flask import Flask, request, jsonify
+from flask import Flask
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from api import amenities, cities, places, reviews, users
 from persistence.data_manager import DataManager
-import api.amenities
-import api.cities
-import api.places
-import api.reviews
-import api.users
-
 
 def main():
-
     app = Flask(__name__)
-    if __name__ == '__main__':
-        app.run(debug=True)
+
+    # Initialize DataManager
+    data_manager = DataManager()
+
+    # Register blueprints for different APIs
+    app.register_blueprint(amenities)
+    app.register_blueprint(cities)
+    app.register_blueprint(places)
+    app.register_blueprint(reviews)
+    app.register_blueprint(users)
+
+    return app
+
+if __name__ == '__main__':
+    app = main()
+    app.run(debug=True)
